c426017197fe216c4ab9e690e2a765fa2e62ff7a
Jordan MacDonald <jordan@wastedintelligence.com>
2015-01-16 00:52:57 -0500

    Implement Position and Range structs, with Range validator.

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -1,1 +1,26 @@
 mod gap_buffer;
+
+#[cfg(test)]
+mod tests;
+
+struct Position {
+    line:   u64,
+    offset: u64,
+}
+
+struct Range {
+    start: Position,
+    end:   Position,
+}
+
+impl Range {
+    fn is_valid(&self) -> bool {
+        if self.start.line < self.end.line {
+            true
+        } else if self.start.line == self.end.line && self.start.offset < self.end.offset {
+            true
+        } else {
+            false
+        }
+    }
+}
