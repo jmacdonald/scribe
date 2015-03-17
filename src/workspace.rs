51c79a03c7e2f36ebc5156e5e3ce0524f4e519dc
Jordan MacDonald <jordan@wastedintelligence.com>
2015-03-17 00:59:29 -0400

    Implement next_buffer method for workspace type.

--- src/workspace.rs
+++ src/workspace.rs
@@ -48,6 +48,19 @@ impl Workspace {
             None => return,
         }
     }
+
+    pub fn next_buffer(&mut self) {
+        match self.current_buffer_index {
+            Some(index) => {
+                if index == self.buffers.len()-1 {
+                    self.current_buffer_index = Some(0);
+                } else {
+                    self.current_buffer_index = Some(index+1);
+                }
+            },
+            None => return,
+        }
+    }
 }
 
 pub fn new(path: Path) -> Workspace {
@@ -153,6 +166,44 @@ mod tests {
 
         // Ensure that it wraps back to the third buffer.
         workspace.previous_buffer();
+        assert_eq!(workspace.current_buffer().unwrap().data(), "third buffer");
+    }
+
+    #[test]
+    fn next_buffer_does_nothing_when_no_buffers_are_open() {
+        let mut workspace = new(Path::new("tests/sample"));
+        workspace.next_buffer();
+        assert!(workspace.current_buffer().is_none());
+    }
+
+    #[test]
+    fn next_buffer_when_three_are_open_selects_next_wrapping_to_first() {
+        let mut workspace = new(Path::new("tests/sample"));
+
+        // Create two buffers and add them to the workspace.
+        let mut first_buffer = buffer::new();
+        let mut second_buffer = buffer::new();
+        let mut third_buffer = buffer::new();
+        first_buffer.insert("first buffer");
+        second_buffer.insert("second buffer");
+        third_buffer.insert("third buffer");
+        workspace.add_buffer(first_buffer);
+        workspace.add_buffer(second_buffer);
+        workspace.add_buffer(third_buffer);
+
+        // Ensure that the third buffer is currently selected.
+        assert_eq!(workspace.current_buffer().unwrap().data(), "third buffer");
+
+        // Ensure that it wraps back to the first buffer.
+        workspace.next_buffer();
+        assert_eq!(workspace.current_buffer().unwrap().data(), "first buffer");
+
+        // Ensure that the second buffer is returned.
+        workspace.next_buffer();
+        assert_eq!(workspace.current_buffer().unwrap().data(), "second buffer");
+
+        // Ensure that the third buffer is returned.
+        workspace.next_buffer();
         assert_eq!(workspace.current_buffer().unwrap().data(), "third buffer");
     }
 }
