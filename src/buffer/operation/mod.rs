8369e3fc30d600590c44f3866a8c32ac2e12a055
Jordan MacDonald <jordan@wastedintelligence.com>
2015-07-04 17:59:37 -0400

    Add documentation to operations module and Operation type.

--- src/buffer/operation/mod.rs
+++ src/buffer/operation/mod.rs
@@ -4,6 +4,10 @@ pub use self::group::OperationGroup;
 pub mod group;
 pub mod history;
 
+/// A reversible buffer operation.
+///
+/// Types that implement this trait are responsible for adding methods to the
+/// Buffer type that build, run, and add themselves to the buffer history.
 pub trait Operation {
     fn run(&mut self, &mut Buffer);
     fn reverse(&mut self, &mut Buffer);
--- src/buffer/operations/mod.rs
+++ src/buffer/operations/mod.rs
@@ -1,2 +1,4 @@
+//! This module contains reversible buffer operations,
+//! un/redone using the buffer type's undo/redo methods.
 pub mod insert;
 pub mod delete;
