c0459d12c48048cfb44010dbd088701a201645ca
Jordan MacDonald <jordan@wastedintelligence.com>
2016-11-03 18:20:31 -0400

    Add a benchmark test for buffer tokens

---
+++ benches/buffer_tokens.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate test;
+extern crate scribe;
+
+use test::Bencher;
+use scribe::Workspace;
+use std::path::PathBuf;
+
+#[bench]
+fn bench_tokens(b: &mut Bencher) {
+    // Create a workspace with this benchmark test as a buffer.
+    let mut workspace = Workspace::new(PathBuf::from("."));
+    let path = PathBuf::from("benches/buffer_tokens.rs");
+    workspace.open_buffer(path);
+
+    // Benchmark the buffer's tokens method.
+    let buffer = workspace.current_buffer().unwrap();
+    b.iter(|| buffer.tokens());
+}
