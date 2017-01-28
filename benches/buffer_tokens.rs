0fbcad3eff2f6db8dad0fa66d3fdc7fca2358008
Jordan MacDonald <jordan@wastedintelligence.com>
2017-01-27 23:18:52 -0500

    Update benchmark test to work with workspace result

--- benches/buffer_tokens.rs
+++ benches/buffer_tokens.rs
@@ -5,19 +5,19 @@ extern crate scribe;
 
 use test::Bencher;
 use scribe::Workspace;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[bench]
 fn bench_tokens(b: &mut Bencher) {
     // Create a workspace with this benchmark test as a buffer.
-    let mut workspace = Workspace::new(Path::new("."));
+    let mut workspace = Workspace::new(Path::new(".")).unwrap();
     let path = Path::new("benches/buffer_tokens.rs");
     workspace.open_buffer(path);
 
     // Benchmark the buffer's tokens method.
     let buffer = workspace.current_buffer().unwrap();
     b.iter(|| {
-        let mut tokens = buffer.tokens().unwrap();
+        let tokens = buffer.tokens().unwrap();
 
         // Exhaust the token iterator.
         for _ in tokens.iter() {}
